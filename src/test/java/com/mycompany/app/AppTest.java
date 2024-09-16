6c87f217264700bc208d552df6390b6ea73c7326
Kunal Kumar <158990169+iambuckky007@users.noreply.github.com>
2024-09-16 21:04:04 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello Welcome to the Jenkins we are using java application and maven as a build tool, keep learning enjoy !!!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
+            fail("\"message\" is not \"Hello Welcome to the Jenkins we are using java application and maven as a build tool, keep learning enjoy !!!"");
         }
     }
 
